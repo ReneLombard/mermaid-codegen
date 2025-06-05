817ad321d0d8353e48a676a580f2f10bb91c6f38
djvandijk <djvandijk@users.noreply.github.com>
2025-06-05 08:46:36 +0000

    deploy: 2a09966e39304d32f886f68191a4186a0255de5b

---
+++ assets/chunks/classDiagram-v2-COTLJTTW.DsjrlZDz.js
@@ -0,0 +1,1 @@
+import{s as a,c as s,a as e,C as t}from"./chunk-A2AXSNBT.D3AFjJ7Z.js";import{_ as i}from"../app.D_E7mvqL.js";import"./chunk-RZ5BOZE2.BL5O9FAD.js";import"./framework.D3hQ4SZ3.js";import"./theme.CbCoqzEL.js";var p={parser:e,get db(){return new t},renderer:s,styles:a,init:i(r=>{r.class||(r.class={}),r.class.arrowMarkerAbsolute=r.arrowMarkerAbsolute},"init")};export{p as diagram};
