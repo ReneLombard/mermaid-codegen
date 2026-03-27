626323f8e39852e7ec2d28b1464bc717a36edb18
René Lombard <Rene.Lombard@elekta.com>
2026-03-27 14:54:37 +0100

    Remove legacy step definitions for test workspace setup and target language selection

--- src/features/step_definitions/project-initialization.steps.ts
+++ src/features/step_definitions/project-initialization.steps.ts
@@ -1,24 +1,7 @@
-import { Given, Then } from '@cucumber/cucumber';
+import { Then } from '@cucumber/cucumber';
 import * as assert from 'assert';
 import * as path from 'path';
 import { CustomWorld } from '../support/world';
-
-// Legacy support for existing step patterns
-
-Given('I have a clean test workspace', async function (this: CustomWorld) {
-    // This is handled by the Before hook
-    assert.strictEqual(await this.fileExists(this.workspaceDir), true);
-});
-
-Given('I have selected {string} as the target language', function (this: CustomWorld, language: string) {
-    this.attach(`Selected language: ${language}`);
-    this.testData.language = language;
-});
-
-Given('I have specified {string} as the project directory', function (this: CustomWorld, directory: string) {
-    this.attach(`Project directory: ${directory}`);
-    this.testData.projectDir = directory;
-});
 
 Then('a new project structure should be created', async function (this: CustomWorld) {
     const directory = this.testData.projectDir || 'test-project';
